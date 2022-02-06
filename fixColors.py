b0ff354e7903ca2b07e5b42a1ba837adb9ed0fe8
Matthew <mattbhubbs@knights.ucf.edu>
2022-02-05 21:38:13 -0500

    ran script, fixed colors

--- fixColors.py
+++ fixColors.py
@@ -1,11 +1,9 @@
 import cv2
-from PIL import Image
 
 for i in range(66):
     file_name = "bb8_sample{}.png".format(i)
     cv_image = cv2.imread(file_name)
-    bgr2rgb = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-    real_image = Image.fromarray(bgr2rgb)
-    real_image.save(file_name)
+    fixed_color = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+    cv2.imwrite(file_name, fixed_color)
     print(file_name + " has been coverted!")
     i += 1
